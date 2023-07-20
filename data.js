e41fd39015faee4a0fb5334dcb6ea10f2eaf22e5
Sakshiii-Singhhh <105056983+Sakshiii-Singhhh@users.noreply.github.com>
2023-07-20 20:28:08 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+let movies = [
+  
+    {
+        name: "falcon and the winter soldier",
+        des:"Falcon and the Winter Soldier are a mismatched duo who team up for a global adventure that will test their survival skills -- as well as their patience.",
+        image: "images/slider 2.PNG",
+    },
+    {
+        name: "loki",
+        des: "Loki, the God of Mischief, steps out of his brother's shadow to embark on an adventure that takes place after the events of Avengers: Endgame.",
+        image: "images/slider 1.PNG",
+    },
+    {
+      name: "wanda vision",
+      des:"Living idealized suburban lives, super-powered beings Wanda and Vision begin to suspect that everything is not as it seems.",
+      image: "images/slider 3.PNG",
+    },
+    {
+      name: "raya and the last dragon",
+      des:"Raya, a warrior, sets out to track down Sisu, a dragon, who transferred all her powers into a magical gem which is now scattered all over the kingdom of Kumandra, dividing its people.",
+      image: "images/slider 4.PNG",
+    },
+    {
+      name: "luca",
+      des: "Luca, a sea monster, befriends Alberto, another one of his kind who takes him on a land adventure. He experiences an exciting summer while keeping his parents in the dark.",
+      image: "images/slider 5.PNG",
+    },
+  ];
